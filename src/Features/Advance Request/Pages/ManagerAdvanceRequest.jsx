c64441cccc260ce47db6d0629491bdbe3e2b6ff3
Vivek25898 <vivekawari11@gmail.com>
2026-06-17 15:59:49 +0530

    Regional Head My Request Issue Fixed.

--- src/Features/Advance Request/Pages/ManagerAdvanceRequest.jsx
+++ src/Features/Advance Request/Pages/ManagerAdvanceRequest.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { selectRole } from '../../../Auth/authSlice'
 import SharedAdvanceRequestForm from '../Components/SharedAdvanceRequestForm'
 
+// Derive myRequestsPath from the logged-in role so Regional Head uses /dashboard/regional-head/*
+const MY_REQUESTS_PATH = {
+  'line-manager':  '/dashboard/line-manager/my-requests',
+  'regional-head': '/dashboard/regional-head/my-requests',
+}
+
 const ManagerAdvanceRequest = () => {
-  return (
-    <SharedAdvanceRequestForm
-      role="line-manager"
-      myRequestsPath="/dashboard/line-manager/my-requests"
-    />
-  )
+  const role = useSelector(selectRole)
+  const myRequestsPath = MY_REQUESTS_PATH[role] || '/dashboard/line-manager/my-requests'
+
+  return <SharedAdvanceRequestForm role={role} myRequestsPath={myRequestsPath} />
 }
 
 export default ManagerAdvanceRequest
