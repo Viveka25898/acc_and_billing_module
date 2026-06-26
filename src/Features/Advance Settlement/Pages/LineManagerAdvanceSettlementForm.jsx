3bae8b9d3770afe3bbf195f8716f1cb44010adcd
Vivek25898 <vivekawari11@gmail.com>
2026-06-26 10:36:38 +0530

    Advance Settlement:- My Requests for Operation Executive, Supervisor etc have been fixed,

--- src/Features/Advance Settlement/Pages/LineManagerAdvanceSettlementForm.jsx
+++ src/Features/Advance Settlement/Pages/LineManagerAdvanceSettlementForm.jsx
@@ -1,12 +1,20 @@
 import SharedAdvanceSettlementForm from '../Components/SharedAdvanceSettlementForm'
+import { useSelector } from 'react-redux'
+import { selectRole } from '../../../Auth/authSlice'
 
 const LineManagerAdvanceSettlementForm = () => {
+  const role = useSelector(selectRole)
+  const mySettlementsPath = role === 'regional-head'
+    ? '/dashboard/regional-head/my-settelment-requests'
+    : '/dashboard/line-manager/my-settelment-requests'
+
   return (
     <SharedAdvanceSettlementForm
-      role="line-manager"
-      mySettlementsPath="/dashboard/line-manager/my-settelment-requests"
+      role={role || 'line-manager'}
+      mySettlementsPath={mySettlementsPath}
     />
   )
 }
 
 export default LineManagerAdvanceSettlementForm
+
